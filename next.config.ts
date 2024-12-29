4d9742e297cca19673e36407e1341d42bc3ff28f
Frederike Falke <frederike_falke@hotmail.de>
2024-12-29 11:35:32 -0500

    fix next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost']
+  },
+  // Add any other config options you need
 };
 
 export default nextConfig;
